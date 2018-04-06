e9ad6567bed8bdd0d79e91a9e608b5e769ab2107
Anthony Rodriguez <ar358@duke.edu>
2018-04-05 21:59:53 -0400

    uploaded databse and created income table

---
+++ SQL Create Files/income.sql
@@ -0,0 +1,11 @@
+CREATE TABLE income 
+(
+    county,
+    "2012" character varying(15),
+    "2013" character varying(7),
+    "2014" character varying(7),
+    "2015" character varying(7),
+    "2016" character varying(7),
+    "2017" character varying(7)
+
+);
